557f2102b177ddb02f9ae4e6d84465c7befaae37
ozawa <ozwtkm3062@gmail.com>
2018-08-30 21:47:26 +0900

    server.rb 参戦！

---
+++ server.rb
@@ -0,0 +1,27 @@
+require 'webrick'
+include WEBrick
+
+module WEBrick::HTTPServlet
+  FileHandler.add_handler('rb', CGIHandler)
+end
+
+# httpサーバー
+s = HTTPServer.new(:DocumentRoot => '/var/www/html/testruby/', :Port => 8082 )
+
+
+# サーブレット
+class HelloServlet < HTTPServlet::AbstractServlet
+  def do_GET(req, res)
+    res['Content-Type'] = "text/html"
+    res.body = "<html><body>hello world.</body></html>"
+  end
+end
+
+# サーブレットをマウント
+s.mount("/hello", HelloServlet)
+
+# Ctrl + C で停止するようにトラップを仕込む。
+trap(:INT){ s.shutdown }
+
+# サーバーを起動
+s.start
