f2df142d1d1862f13916a4f9e38b8ea06826eff6
ozawa <ozwtkm3062@gmail.com>
2018-10-14 01:22:05 +0900

    Baseクラス作ってそれっぽくした

--- regist.rb
+++ regist.rb
@@ -7,7 +7,7 @@ require 'digest/sha1'
 require 'securerandom'
 
 
-class View
+class Base
 
 def view_header()
 
@@ -56,11 +56,27 @@ def view_body(view_buffer)
 end
 
 
+def validate_special_character(input)
+
+		if input.match(/[0-9a-zA-Z_@-]*/) == nil
+	
+			return false
+		
+		else
+	
+			return true
+	
+		end
+	
+end
+
+
 end
 
 
 
-class Regist
+
+class Regist < Base
 
 
 def check_id_duplication(sql, username, passwd)
@@ -107,37 +123,52 @@ end
 
 
 # 各処理のためのインスタンス生成
-input = CGI.new
+cgi = CGI.new
 sql = Mysql2::Client.new(:socket => '/var/lib/mysql/mysql.sock', :host => 'localhost', :username => 'testwebrick', :password => 'test', :encoding => 'utf8', :database => 'webrick_test')
-view = View.new
 regist = Regist.new
 
+view_buffer = ""
 
-
-if input.request_method == "POST" then
+# メイン処理
+if cgi.request_method == "POST" then
 
 	# POSTされた値をinsertする。
-	username = input["name"]
-	passwd = input["passwd"]
-	exist_count = nil
+	username = cgi["name"]
+	passwd = cgi["passwd"]
 
-	if !regist.check_id_duplication(sql, username, passwd)
-	
-		view_buffer += "キャラ被りｗ"
-		
-	else 
-	
-		regist.regist(sql, username, passwd)
-		view_buffer += "#{username}を登録しといたぞ。"
-		
+	# 入力値がすべてvalidate_special_characterをtrueとしたときのみ登録処理に進みたい。
+	# なんかここ気に食わない
+	validate_result = true
+	[username,passwd].each do |row|
+		if !regist.validate_special_character(row) then
+			view_buffer += "#{row}に特殊文字入れるな<br>"
+			validate_result　= false
+		end
 	end
-
+	
+	
+	# 登録処理。
+	if validate_result then 
+		if !regist.check_id_duplication(sql, username, passwd)
+	
+			view_buffer += "キャラ被りｗ"
+		
+		else 
+	
+			regist.regist(sql, username, passwd)
+			view_buffer += "#{username}を登録しといたぞ。"
+		
+		end
+	end
+			
 else
 
-	view_buffer += "GETだね"
+	view_buffer += "このフォームから登録してね～～"
 	
 end
 
-view.view_body(view_buffer)
-view.view_footer()
+
+regist.view_header()
+regist.view_body(view_buffer)
+regist.view_footer()
 
