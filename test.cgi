8d8b65c807470945dfe7ea435f2a8e0f964916ef
ozawa <ozwtkm3062@gmail.com>
2018-10-20 18:47:38 +0900

    いらないの削除

--- test.cgi
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/ruby
-print "Content-type: text/html\n\n"
-
-print "<html><body>Hello World</body></html>"
--- webrick.rb
+++
@@ -1,9 +0,0 @@
-require 'webrick'
-
-srv = WEBrick::HTTPServer.new({
-  DocumentRoot:   './',
-  BindAddress:    192.168.119.128,
-  Port:           88,
-})
-
-srv.start
