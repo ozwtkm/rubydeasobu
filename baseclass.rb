63b2cb78df2835fe474d0f1f8ea1d3d80817c62f
ozawa <ozwtkm3062@gmail.com>
2019-01-14 18:24:21 +0900

    clean: いらん部分をさくっと削除

--- baseclass.rb
+++ baseclass.rb
@@ -1,5 +1,6 @@
 ﻿require 'webrick'
 require 'mysql2'
+require_relative './render'
 
 class Base
 
@@ -7,20 +8,18 @@ METHOD_GET = 0
 METHOD_POST = 1
 RESULT_SPECIAL_CHARACTER_ERROR = 0
 
+
 def initialize(req,res)
 
 	@req = req
 	@res = res
-
-end
-
-
-def create_instance()
 	
 	@sql = Mysql2::Client.new(:socket => '/var/lib/mysql/mysql.sock', :host => 'localhost', :username => 'testwebrick', :password => 'test', :encoding => 'utf8', :database => 'webrick_test')
-  
+	
+	# view時、テンプレートに渡すための変数(ハッシュ)の箱 
+	@context = {}
+	
 end
-
 
 
 def view(status={})
@@ -29,7 +28,6 @@ def view(status={})
 	view_http_body(status)
 	
 end
-
 
 
 def view_http_header()
@@ -67,26 +65,21 @@ def validate_special_character(input_hash)
 			
 	end
 	
-	return true
-	
-end
-
-
-def add_break()
-
-	@res.body += "<br>\r\n"
-
 end
 
 
 # オーバーライドするぜんてい
 def get_handler()
 	
+	view()
+	
 end
 
 
 # オーバーライドするぜんてい
 def post_handler()
+
+	view()
 
 end
 
@@ -103,7 +96,7 @@ end
 
 
 
-
+# 入力値に特殊記号が来たときに使うエラー
 class Special_character_error < StandardError
 attr_reader :falselist
 
@@ -112,5 +105,8 @@ def initialize(list)
 	@falselist = list
 
 end
+
 end
+
+
 
