a8af032cbd1776b4e3378fc8176a6c4d4a24e69a
ozawa <ozwtkm3062@gmail.com>
2019-02-02 15:28:15 +0900

    clean: websocketまわりをせいり

--- websocket.rb
+++ websocket.rb
@@ -1,88 +1,86 @@
 ﻿#!/usr/bin/ruby -Ku
 # -*- coding: utf-8 -*-
 
+
 require 'em-websocket'
 require 'cgi'
 require 'cgi/session'
-require 'pry'
+require_relative './baseclass'
 
-# セッション変数をwebsocketのインスタンスに渡すためにem-websocketをいじる
+# セッション変数をwebsocketのインスタンスに渡すためにem-websocketをいじり、
+# 
 module Ex_connection
- refine EventMachine::WebSocket::Connection do
-   attr_accessor :username
-  
-   def set_username(name)
-     @username = name
-   end
- end
+	refine EventMachine::WebSocket::Connection do
+	
+		attr_accessor :username
+
+		def set_username(name)
+			@username = name
+		end
+		
+	end
+	
 end
 
+# オフラインモード回避のためのおまじない
 ENV['REQUEST_METHOD'] = 'GET'
-
 
 connections = []
 
+
 EM.run {
-  using Ex_connection
-  EM::WebSocket.run(:host => "127.0.0.1", :port => 8882) do |ws|
-     ws.onopen { |handshake|
+	using Ex_connection
+	EM::WebSocket.run(:host => "127.0.0.1", :port => 8882) do |ws|
+		ws.onopen { |handshake|
 
-	 cgi = CGI.new
-	 
-	 c = handshake.headers_downcased["cookie"]
-	 puts handshake.instance_variable_get(:@headers)
-	 sessionkeyvalue = c.split('=')
-	 puts sessionkeyvalue
-	 h = {}
-	 
-	 
-	 sessionid = sessionkeyvalue[1]
-	 
-	puts sessionid
-	 cgi.cookies['_session_id'] = sessionid
-	 
-	 session = CGI::Session.new(cgi, {"new_session" => false, "session_id" => sessionid})
-	 
-	 puts session.instance_variables
-	 puts session['name']
-	  
-      puts "WebSocket connection open"
+			cgi = CGI.new
+		 
+			c = handshake.headers_downcased["cookie"]
+			sessionkeyvalue = c.split('=')
+			h = {}
+			sessionid = sessionkeyvalue[1]
+		 
+			cgi.cookies['_session_id'] = sessionid
+		
+			session = CGI::Session.new(cgi, {'new_session' => false})
 
-      ws.send "Hello #{session['name']} , you connected to #{handshake.path}"
-	  
-	  connections << ws  
-      ws.set_username(session['name'])
-	  
-	  p connections
-	  
-	  connections.each{|conn|
-		 conn.send("#{ws.username} 参戦！！")
-	  }
-    }
+			ws.set_username(session['name'])
 
+			ws.send "Hello #{ws.username} , you connected to #{handshake.path}"
 
-    ws.onmessage { |msg|
-      puts "Recieved message: #{msg}"
-	  p connections
+			puts "#{ws.username}が来たみたいだよ"  
+		  
+			connections.each{|conn|
+				conn.send("#{ws.username} 参戦！！")
+		  }
 
-	  
-	  connections.each{|conn|
-		conn.send("#{ws.username} Said : #{msg}")
-	  }
-    }
-	
-	ws.onclose {
-	
-	p "close"
-	
-		puts "#{ws.username} ga kaecchattamitai"
-		connections.each{|conn|
-			conn.send("#{ws.username}が尻尾を巻いて逃げ出した")
+			connections << ws
+		
 		}
-	}
+
+		ws.onmessage { |msg|
+		
+			puts "Recieved message: #{msg} from #{ws.username}"
+	  
+			connections.each{|conn|
+				conn.send("#{ws.username} Said : #{msg}")
+			}
+			
+		}
 	
 	
-  end
+		ws.onclose {
+		
+			puts "#{ws.username} が帰宅したよ"
+			
+			connections.each{|conn|
+				conn.send("#{ws.username}が尻尾を巻いて逃げ出した")
+			}
+			
+		}
+	
+	end
+
 }
 
 
