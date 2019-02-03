7a5b40cbe1130e9e6e8bb9d72e3116064cae61f5
ozawa <ozwtkm3062@gmail.com>
2019-02-03 19:06:56 +0900

    clean: 処理を関数化&sessionidのとり方をsplit→正規表現にした

--- websocket.rb
+++ websocket.rb
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/ruby -Ku
+#!/usr/bin/ruby -Ku
 # -*- coding: utf-8 -*-
 
 
@@ -14,10 +14,6 @@ module Ex_connection
 	
 		attr_accessor :username
 
-		def set_username(name)
-			@username = name
-		end
-		
 	end
 	
 end
@@ -29,59 +25,90 @@ connections = []
 
 
 EM.run {
+
 	using Ex_connection
+	
 	EM::WebSocket.run(:host => "127.0.0.1", :port => 8882) do |ws|
+	
 		ws.onopen { |handshake|
 
-			cgi = CGI.new
-		 
-			c = handshake.headers_downcased["cookie"]
-			sessionkeyvalue = c.split('=')
-			h = {}
-			sessionid = sessionkeyvalue[1]
-		 
-			cgi.cookies['_session_id'] = sessionid
-		
-			session = CGI::Session.new(cgi, {'new_session' => false})
+			set_session(ws, handshake)	# cgiで使っているセッションの情報をwebsocketに引き継ぐ
 
-			ws.set_username(session['name'])
-
-			ws.send "Hello #{ws.username} , you connected to #{handshake.path}"
-
-			puts "#{ws.username}が来たみたいだよ"  
-		  
-			connections.each{|conn|
-				conn.send("#{ws.username} 参戦！！")
-		  }
+			messege_send("join", ws, connections)
 
 			connections << ws
 		
 		}
 
+
 		ws.onmessage { |msg|
+		
+			messege_send("speak", ws, connections, msg)
+			
+		}
+	
+	
+		ws.onclose {
+
+			messege_send("logout", ws, connections)
+			
+		}
+	
+	end
+	
+	
+	def set_session(ws, handshake)
+	 	 
+		cgi = CGI.new
+	 
+		c = handshake.headers_downcased["cookie"]
+	 
+		cgi.cookies['_session_id'] = c.match(/session_id=([a-f0-9]+)/)[1]
+	
+		session = CGI::Session.new(cgi, {'new_session' => false})
+
+		ws.username = (session['name'])
+
+	end
+
+
+	def messege_send(kind, ws, connections, msg={})
+	
+		case kind
+		when "join" then
+		
+			puts "#{ws.username}が来たみたいだよ"  
+		
+			ws.send "Hello #{ws.username}"
+		  
+			connections.each{|conn|
+				conn.send("#{ws.username} 参戦！！")
+		  }
+		  
+		when "speak" then
 		
 			puts "Recieved message: #{msg} from #{ws.username}"
 	  
 			connections.each{|conn|
 				conn.send("#{ws.username} Said : #{msg}")
 			}
-			
-		}
-	
-	
-		ws.onclose {
 		
+		when "logout" then
+				
 			puts "#{ws.username} が帰宅したよ"
 			
 			connections.each{|conn|
 				conn.send("#{ws.username}が尻尾を巻いて逃げ出した")
 			}
-			
-		}
+		
+		end
 	
 	end
 
 }
+
+
+
 
 
 # 参考そーす：https://github.com/igrigorik/em-websocket
