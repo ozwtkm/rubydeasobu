c05a7505ceaf6b8062777b05fa5978aba39266d1
ozawa <ozwtkm3062@gmail.com>
2019-02-10 02:27:48 +0900

    clean: フォルダをちょいいじり

---
+++ _util/render.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby -Ku 
+# -*- coding: utf-8 -*-
+
+class Base
+
+def render(template, context={})
+
+	# 単純に「context」だと長いから代入して変数名を短くしてるだけ。
+	c = context
+	erb = ERB.new(File.read("/var/www/html/testruby/template/" + template))
+	return erb.result(binding)
+	
+end
+
+end
+
+
