4d5f62d45d9d68a2f83f885971c89b772098e0ac
ozawa <ozwtkm3062@gmail.com>
2019-02-10 21:51:35 +0900

    clean: DB側でunique制約つけたのでcheck_id_duplicationをkill

--- controller/regist.rb
+++ controller/regist.rb
@@ -23,7 +23,6 @@ end
 
 def control()
 
-
 		# 何はともあれまずは入力値検証
 		begin
 			
@@ -43,8 +42,8 @@ def control()
 
 		
 		begin
-		
-			check_id_duplication(@req.query["name"], @req.query["passwd"])
+			
+			@user.regist(@sql, @req.query["name"], @req.query["passwd"])
 		
 		rescue => e
 		
@@ -53,31 +52,10 @@ def control()
 			return
 
 		end
-
-
-		@user.regist(@sql, @req.query["name"], @req.query["passwd"])
 		
 		@context[:msg] << "#{@req.query["name"]}を登録したったで。"
 
 end
-
-
-
-def check_id_duplication(username, passwd)
-
-	# ユーザIDを重複チェック
-	# DB側でunique制約しないとレースコンディションの可能性あり
-	statement = @sql.prepare("select * from users2 where name = ? limit 1")
-	result_tmp = statement.execute(username)
-
-	if result_tmp.count == 1
-
-		raise
-	
-	end
-
-end
-
 
 
 end
