120ddd0ffeb8282fb47996ffd7c8ba01cc26f57d
ozawa <ozwtkm3062@gmail.com>
2019-02-10 17:25:13 +0900

    clean: check_ID_PWを修正

--- model/login_model.rb
+++ model/login_model.rb
@@ -10,27 +10,26 @@ class Login_model
 
 def check_ID_PW(sql, username, passwd)
 	
-	# ログイン可能な入力組み合わせかチェックする。（入力値組に合致するレコードの個数を返す）
 	statement = sql.prepare("select salt from users2 where name = ? limit 1")
 	result_tmp = statement.execute(username)
-	
+		
 	result = nil
 	result_tmp.each do |row|
 		result = row
 	end
 	
+	if result.nil?
+	
+		raise
+	
+	end
+	
 	pw_hash = Digest::SHA1.hexdigest(passwd + result["salt"])
 	
 	statement = sql.prepare("select * from users2 where name = ? and passwd = ? limit 1")
-	result_tmp = statement.execute(username, pw_hash)
-
-	result=nil
-	result_tmp.each do |row|
-		result = row
-	end
+	result = statement.execute(username, pw_hash).count
 	
-	
-	if result.nil?
+	if result == 0
 	
 		raise
 	
