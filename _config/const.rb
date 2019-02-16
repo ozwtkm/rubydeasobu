ec69e4ed2840532da2174d0f1e145d4da274c77d
ozawa <ozwtkm3062@gmail.com>
2019-02-16 16:21:11 +0900

    :clean constsfakusei

---
+++ _config/const.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby -Ku 
+# -*- coding: utf-8 -*-
+
+
+
+PATH = "/var/www/html/testruby/template/"
--- _util/render.rb
+++ _util/render.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby -Ku 
 # -*- coding: utf-8 -*-
 
+require_relative '../_config/const'
+
 class Base
 
 def render(template, context={})
 
 	# 単純に「context」だと長いから代入して変数名を短くしてるだけ。
 	c = context
-	erb = ERB.new(File.read("/var/www/html/testruby/template/" + template))
+	erb = ERB.new(File.read(PATH + template))
 	return erb.result(binding)
 	
 end
