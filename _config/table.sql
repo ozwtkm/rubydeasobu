57ef4fc15fd2e02700e6ba36a9be02dd5ee4eead
ozawa <ozwtkm3062@gmail.com>
2019-03-03 15:38:05 +0900

    update: 外部ユニーく制約とかつけた

--- _config/table.sql
+++ _config/table.sql
@@ -1,54 +1,4 @@
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `transaction` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `transaction`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gacha_probability` (
-  `gacha_id` int(11) unsigned DEFAULT NULL,
-  `monster_id` int(11) DEFAULT NULL,
-  `probability` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gachas` (
-  `gacha_id` int(11) NOT NULL AUTO_INCREMENT,
-  `gacha_name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`gacha_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_monster` (
-  `user_id` int(11) unsigned NOT NULL,
-  `monster_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `salt` varchar(255) DEFAULT NULL,
-  `passwd` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unko` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wallets` (
-  `user_id` int(11) NOT NULL,
-  `money` int(10) unsigned DEFAULT NULL,
-  `gem` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
-
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `master` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `master`;
@@ -56,14 +6,16 @@ USE `master`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gacha_probability` (
   `gacha_id` int(11) unsigned DEFAULT NULL,
-  `monster_id` int(11) DEFAULT NULL,
-  `probability` int(10) DEFAULT NULL
+  `monster_id` int(11) unsigned NOT NULL,
+  `probability` int(11) unsigned NOT NULL,
+  KEY `gacha_id` (`gacha_id`),
+  CONSTRAINT `gacha_probability_ibfk_1` FOREIGN KEY (`gacha_id`) REFERENCES `gachas` (`gacha_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gachas` (
-  `gacha_id` int(11) NOT NULL AUTO_INCREMENT,
+  `gacha_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `gacha_name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`gacha_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
@@ -71,7 +23,7 @@ CREATE TABLE `gachas` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `imgs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -79,7 +31,7 @@ CREATE TABLE `imgs` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `monsters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
   `hp` int(10) unsigned DEFAULT NULL,
   `atk` int(10) unsigned DEFAULT NULL,
@@ -90,4 +42,40 @@ CREATE TABLE `monsters` (
   `rarity` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `transaction` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_monster` (
+  `user_id` int(11) unsigned NOT NULL,
+  `monster_id` int(10) unsigned NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `monster_id` (`monster_id`),
+  CONSTRAINT `user_monster_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_monster_ibfk_2` FOREIGN KEY (`monster_id`) REFERENCES `master`.`monsters` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `passwd` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unko` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wallets` (
+  `user_id` int(11) unsigned NOT NULL,
+  `money` int(10) unsigned DEFAULT NULL,
+  `gem` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `wallets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
