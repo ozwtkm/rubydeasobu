e9f31725493f90eae3c63fdbdc6e98314dee9a5e
ozawa <ozwtkm3062@gmail.com>
2019-04-07 15:02:06 +0900

    clean: しんぐるとんご

---
+++ _util/SQL_transaction.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -Ku 
+# -*- coding: utf-8 -*-
+
+require 'singleton'
+require 'mysql2'
+
+class SQL_transaction
+attr_reader :sql
+
+	include Singleton
+	
+	def initialize
+  
+		@sql = Mysql2::Client.new(:socket => '/var/lib/mysql/mysql.sock', :host => 'localhost', :username => 'testwebrick', :password => 'test', :encoding => 'utf8', :database => 'transaction')
+
+	end
+	
+end
