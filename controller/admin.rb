2d0b2a49cf009fb9307db195fcd178de208efe9a
ozawa <ozwtkm3062@gmail.com>
2019-06-08 13:22:17 +0900

    ロジックは変えてないが体裁的にきれいにした

--- controller/admin.rb
+++ controller/admin.rb
@@ -3,7 +3,7 @@
 require 'json'
 require_relative './_baseclass_require_login'
 require_relative '../model/map'
-require_relative '../util/map_util'
+require_relative '../_util/map_util'
 
 class Admin_controller < Base_require_login
 
@@ -14,49 +14,56 @@ end
 
 def control()
 	#後々リクエストにダンジョンIDとフロア（z）も含める。
-	# ↓リクエストで["3",{"1","1","1","1","1","1","1","1","1","1","1","1"}]みたいなのくる
+	# ↓リクエストで["1","1","3",["1","1","1","1","1","1","1","1","1","1","1","1"]]みたいなのくる
 	data = JSON.parse(@req.body)
 	
-	@num = data[0]
-	@refs= data[1]
-	@dangeon_id = dataからとってくる
-	@side_refs = []
-	@vertical_refs = []
-	require_refs_count = 2*num*(num-1)
+	@dangeon_id = data[0].to_i
+	@floor =  data[1].to_i
+	@num = data[2].to_i
+	@refs= data[3]	
+
+	@side_refs = [[]]
+	@vertical_refs = [[]]
+	require_refs_count = 2*@num*(@num-1)
+	
+	if @num < 2 || @num > 100 # まあここは決めの問題ではある
+		raise
+	end
 	
 	if @refs.count != require_refs_count
 		raise
 	end
-
+	
 	shape_refs()
 
 	map = Map_util.create(@side_refs,@vertical_refs) # Maputilの中のcreate関数にmarkandsweepとmapobj生成がらっぷされる
 	
 	Map.add_by_instance(map)
-	
 end
 
 
 #スマートなやり方あとで考える
 def shape_refs
 	line=0
-	num-1.times do
-			num-1.times do 
-				@side_refs[line].push(refs.shift)
-			end
-			
-			num.times do
-				@vertical_refs[line].push(refs.shift)
-			end
-			
-			line+=1
+
+	(@num-1).times do
+		(@num-1).times do 
+			@side_refs[line].push(@refs.shift.to_i)
+		end
+		@num.times do
+			@vertical_refs[line].push(@refs.shift.to_i)
+		end
+		
+		line+=1
+		@side_refs[line] = []
+		@vertical_refs[line] = []
+	end
+
+	(@num-1).times do
+		@side_refs[line].push(@refs.shift.to_i)
 	end
 	
-	num-1.times do
-		@side_refs[line].push(refs.shift)
-	end
-	
-	if @side_refs[line].nil? || !@refs.last.nil?
+	if @side_refs[line][@num-2].nil? || !@refs.last.nil?
 		raise
 	end
 	
