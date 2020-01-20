3fa7925a0dca0e88751ab85eac97a9fd6317c1db
ozawa <ozwtkm3062@gmail.com>
2020-01-20 23:04:27 +0900

    clean: registの処理をmodel側に引っ越し

--- model/user.rb
+++ model/user.rb
@@ -1,23 +1,24 @@
 #!/usr/bin/ruby -Ku
 # -*- coding: utf-8 -*-
 
-require_relative '../_util/SQL_transaction'
+require_relative '../_util/sqltool'
 require_relative '../exception/Error_duplicate_id'
 require_relative './basemodel'
+require_relative './monster'
+require_relative './wallet'
+require_relative './party'
+
 
 class User < Base_model
 	attr_reader :id, :name
 
 def initialize(userinfo)
-
 	@id = userinfo["id"]
 	@name = userinfo["name"]
-
 end
 
 
 def self.get_user(user_id)
-
 	sql_transaction =  SQL_transaction.instance.sql
 
 	statement = sql_transaction.prepare("select * from users where id = ? limit 1 for update")
@@ -30,36 +31,32 @@ def self.get_user(user_id)
 	statement.close
 	
 	return user
-
 end
 
 
-def self.add_user(username, passwd)
 
-	sql_transaction =  SQL_transaction.instance.sql
-
+def self.add(username, passwd)
 	salt = SecureRandom.hex(10) + "aaaaburiburi"
 		
 	pw_hash = Digest::SHA1.hexdigest(passwd+salt)
 	
-	statement = sql_transaction.prepare("insert into users(name,salt,passwd) values(?,?,?)")
-
 	begin
-
-		statement.execute(username, salt, pw_hash)
-
+		SQL.transaction("insert into users(name,salt,passwd) values(?,?,?)", [username, salt, pw_hash])
 	rescue
-
 		raise Error_duplicate_id.new
-	
 	end
 
-	user = User.new({"id"=>sql_transaction.last_id,"name"=>username})
+	user_id = SQL_transaction.instance.sql.last_id
 
-	statement.close
-		
-	return user
+	user = User.new({"id"=>user_id, "name"=>username})
 
+	Wallet.init(user_id)
+	initial_monster = Monster.init(user_id)
+	Party.init(user_id, initial_monster)
+
+	SQL.close_statement
+
+	user
 end
 
 
