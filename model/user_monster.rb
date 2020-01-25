47ce5a0ac310ddbdf957c0f4742aec5551c3d572
ozawa <ozwtkm3062@gmail.com>
2020-01-25 15:09:57 +0900

    add: user_monsterモデルを作成。（cache乗せるのはやめた）

--- model/user_monster.rb
+++ model/user_monster.rb
@@ -22,11 +22,11 @@ end
 
 
 def self.get_possession_monsters(user_id, limit:10, offset:0)
-    possession_monster_list = []
-
-    user_monster_list = User_monster.get_possesion_info(user_id, limit:limit, offset:offset)
+    user_monster_list = SQL.transaction("select * from user_monster where user_id = ? limit ? offset ?", [user_id,limit,offset])
+    Validator.validate_SQL_error(user_monster_list.count, is_multi_line: true)
 
     user_monster_info = {}
+    possession_monster_list = []
     user_monster_list.each do |row|
         user_monster_info["id"] = row["id"]
         user_monster_info["user_id"] = row["user_id"]
@@ -35,34 +35,24 @@ def self.get_possession_monsters(user_id, limit:10, offset:0)
         possession_monster_list << User_monster.new(user_monster_info)
     end
 
+    SQL.close_statement
+
     possession_monster_list
 end
 
 
-def self.get_possesion_info(user_id, limit:10, offset:0)
-    range = offset..(limit-1)
-    array_for_identify_cached = Array.new(limit)
-    user_monster_list = []
-
-    # ＊取ってきたいrange内にキャッシュがあればそこは除いてSQL文を発行するようにしたい
-    #  ＊range内にキャッシュがあるパターンは6種類あり、それぞれで発行すべきSQLが変わる
-    #    1　冒頭n個がキャッシュされてる　→ 1~nを除外した範囲でSQLを発行
-    #    2  最後尾n個がキャッシュされてる　→　-(1~n)を除外した範囲でSQLを発行
-    #    3  冒頭n個と最後尾m個がキャッシュされてる　→　1~nと-(1~m)を除外した範囲でSQLを発行
-    #    4  全部がキャッシュされてる　→　SQLの発行自体不要
-    #    5  全くキャッシュなし　→　普通にそのままの範囲でSQL発行
-    #    6  上記どれにも当てはまらない形で飛び飛びにキャッシュされてる　→　細かい範囲のSQLを複数発行と広い範囲のSQLを1発発行するのはどっちが重いんだ？
-    range.each do |row|
-        unless Cache.instance.get('user_monster:' + user_id.to_s + ':offset:' + row.to_s).nil?
-            array_for_identify_cached[(row-offset)] = true
-        end
+def self.get_specific_user_monster_info(user_monster_id, user_id=nil)
+    if user_id.nil?
+        specific_user_monster_info = SQL.transaction("select * from user_monster where id = ? limit 1", user_monster_id)
+    else
+        specific_user_monster_info = SQL.transaction("select * from user_monster where id = ? and user_id = ? limit 1", [user_monster_id, user_id])
     end
 
-    # 上記6パターンに応じた処理の分岐をする（sql発行→cacheset→usermonsterモデルのリストを生成しuser_monster_listを構築）
-    if array_for_identify_cached ~~~~
-    elsif ~~~
+    Validator.validate_SQL_error(specific_user_monster_info.count, is_multi_line: false)
 
-    user_monster_list
+    SQL.close_statement
+
+    specific_user_monster_info[0]
 end
 
 
